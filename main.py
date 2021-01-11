dfcb916ca89326b34221c6f928e9ffe62e4ab40a
Tobias Hornberger <th@craftnote.de>
2021-01-11 08:22:38 +0100

    Use an array of lamp IDs

--- main.py
+++ main.py
@@ -6,19 +6,27 @@ from google.cloud import pubsub_v1
 # Disable unverified SSL certificate warning
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+# TODO use this: https://cloud.google.com/community/tutorials/delivering-cloud-monitoring-notifications-to-third-party-services
+# TODO read from config
+
 project_id = "craftnote-live"
 subscription_name = "projects/craftnote-live/subscriptions/cloud-function-errors"
 bridgeIp = "192.168.1.113"
 username = "lx-2rUum1alLQS9ncdvxbAoROxKdgv7bsTRsKSl5"
-lampId = "6"  # 5: living room, 6: kitchen
-
-# https://192.168.1.113/api/lx-2rUum1alLQS9ncdvxbAoROxKdgv7bsTRsKSl5/lights/6/state
+lampIds = ["18","19"]  # 5: living room, 6: kitchen, 18 studio links
 
 count = 0
 
-def set_light(value):
-    hue = max(0, 10 - value) * 10000;
+def set_lights(value):
+    min_value = 0
+    max_value = 5
+    scaling_factor = 4000
+    hue = max(min_value, max_value - value) * scaling_factor
 
+    for lampId in lampIds:
+        set_lamp_color(lampId, hue)
+
+def set_lamp_color(lampId, hue):
     url = f"https://{bridgeIp}/api/{username}/lights/{lampId}/state"
     body = {"on": True, "sat": 254, "bri": 254, "hue": hue}
     r = requests.put(url, json=body, verify=False)
@@ -43,7 +51,7 @@ def subscribe():
 def run():
     global count
     print(f"The count was {count}")
-    set_light(count)
+    set_lights(count)
     count = 0
     threading.Timer(10.0, run).start()
 
